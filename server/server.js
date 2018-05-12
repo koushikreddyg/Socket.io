47978caec308c0d432a4a3574c9195fcff2a6681
koushikui <koushikreddygunapati@gmail.com>
2018-05-12 16:40:33 -0500

    greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -17,13 +17,26 @@ app.use(express.static(publicPath))
 
 io.on('connection',(socket)=>{
     console.log('new user connected');
-   
+    socket.emit('newMessage',{
+        from: "Admin",
+        text: 'Welcome to chat-app'
+    })
+    socket.broadcast.emit('newMessage',{
+        from: "Admin",
+        text: 'new User joined',
+        createdAt: new Date().getTime()
+    })
     socket.on('createMessage',(message)=>{
         console.log('createMessage', message);
-        io.emit('newMessage',{
+        // io.emit('newMessage',{
+        //     from: message.from,
+        //     message: message.text,
+        //     createAt: new Date().getTime()
+        // })
+        socket.broadcast.emit('newMessage',{
             from: message.from,
-            message: message.message,
-            createAt: new Date().getTime()
+            text: message.text,
+            createdAt: new Date().getTime()
         })
     })
     socket.on('disconnect',()=>{
